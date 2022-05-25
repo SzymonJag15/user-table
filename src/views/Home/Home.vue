a474772c8f315b14e3d57dd181f2958cb90a7b38
szymon <szymon.jagielski@olicom.pl>
2022-05-25 17:16:43 +0200

    Create modal with basic logic

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,24 +1,40 @@
 <template>
   <app-ui-wrapper>
-    <p>{{ title }}</p>
+    <button @click="toggleModal" class="Home__show-modal">toggleModal</button>
   </app-ui-wrapper>
+  <app-modal
+    :isVisible="visibleModal"
+    :image="ModalImage"
+    title="Gratulujemy!"
+    text="Test końcowy został zaliczony. Możesz już pobrać swój imienny Certyfikat!"
+    buttonText="Pobierz certyfikat za to szkolenie!"
+    hasButton
+    @close-modal="toggleModal"
+  />
 </template>
 
 <script>
 import { ref } from "vue";
 
 import AppUiWrapper from "@/components/AppUiWrapper/AppUiWrapper.vue";
+import AppModal from "@/components/AppModal/AppModal.vue";
+
+import ModalImage from "@/assets/modal-image.png";
 
 export default {
   name: "Home",
   components: {
     AppUiWrapper,
+    AppModal,
   },
   setup() {
-    const title = ref("test");
+    const visibleModal = ref(true);
+    const toggleModal = () => (visibleModal.value = !visibleModal.value);
 
     return {
-      title,
+      visibleModal,
+      toggleModal,
+      ModalImage,
     };
   },
 };
