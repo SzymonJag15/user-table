73fa067ffdb2ed0c6afd083a19447090214da4f7
SzymonJag15 <69639056+SzymonJag15@users.noreply.github.com>
2022-05-31 16:02:29 +0200

    Fun with animations (#4)
    Co-authored-by: szymon <szymon.jagielski@olicom.pl>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
 <template>
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="scale" mode="out-in">
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
+
+<style scoped>
+.scale-enter-active,
+.scale-leave-active {
+  transition: all 0.5s ease;
+}
+
+.scale-enter-from,
+.scale-leave-to {
+  opacity: 0;
+  transform: scale(0.9);
+}
+</style>
